32a03391d0c4085fb176481272ca8fa3c5001815
Jaith Rajapaksha <jsathnidu@gmail.com>
2025-03-02 21:15:46 +0530

    Update Members.tsx

--- common/sideandnavbar/src/pages/Members/Members.tsx
+++ common/sideandnavbar/src/pages/Members/Members.tsx
@@ -89,3 +89,70 @@ const CustomPagination: React.FC<{
     </Box>
   );
 };
+const TeamSection: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  
+  const totalMembers = 39;
+  
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    setCurrentPage(1); 
+  };
+  
+  return (
+    <Paper 
+      elevation={0} 
+      sx={{ 
+        p: 3, 
+        borderRadius: 4, 
+        backgroundColor: alpha(theme.palette.primary.light, 0.5) 
+      }}
+    >
+      <Box sx={{ 
+        display: 'flex', 
+        justifyContent: 'space-between', 
+        alignItems: 'center', 
+        mb: 3 
+      }}>
+        <Box>
+          <Typography variant="h6" component="span" fontWeight={600} sx={{ mr: 1 }}>
+            UI Developers
+          </Typography>
+          <Typography variant="body1" component="span" color="text.secondary">
+            ({totalMembers})
+          </Typography>
+        </Box>
+        
+        <TextField
+          placeholder="Search members..."
+          value={searchTerm}
+          onChange={handleSearch}
+          size="small"
+          sx={{ width: 300 }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        
+        <Link href="#" underline="none" color="inherit" fontWeight={500}>
+          View all
+        </Link>
+      </Box>
+      
+      <TeamMembersGrid />
+      <TeamMembersGrid />
+      <TeamMembersGrid />
+      
+      <CustomPagination 
+        currentPage={currentPage} 
+        totalPages={3} 
+        onPageChange={setCurrentPage} 
+      />
+    </Paper>
+  );
+};
