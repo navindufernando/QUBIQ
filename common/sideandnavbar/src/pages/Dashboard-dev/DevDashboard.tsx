6f0789a8af784b3aa93c8968af0ce0b73f20365d
Manula <manula.jayabodhi@gmail.com>
2025-03-03 13:55:36 +0530

    Updated side panel styles

--- common/sideandnavbar/src/pages/Dashboard-dev/DevDashboard.tsx
+++ common/sideandnavbar/src/pages/Dashboard-dev/DevDashboard.tsx
@@ -76,7 +76,7 @@ const DevDashboard = () => {
     // >
     <div className="flex">
       {/* Main Section */}
-      <div className="flex-grow p-6">
+      <div className="flex-grow p-6 pt-0">
         <div className="grid grid-cols-2 gap-8">
           <ProjectsCard></ProjectsCard>
           <SkillImprovementCard></SkillImprovementCard>
@@ -95,25 +95,27 @@ const DevDashboard = () => {
         </div>
       </div>
       {/* Side Panel */}
-      <div className="w-1/3 p-6 bg-[#D7C2F2] shadow-md rounded-xl">
-        {/* Calendar */}
-        <h2 className="text-lg font-semibold ml-3">Calendar</h2>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar value={selectedDate} onChange={handleDateChange} />
-        </LocalizationProvider>
+      <div className="w-1/3 ">
+        <div className="p-6 bg-[#D7C2F2] shadow-md rounded-xl h-auto">
+          {/* Calendar */}
+          <h2 className="text-lg font-semibold ml-3">Calendar</h2>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar value={selectedDate} onChange={handleDateChange} />
+          </LocalizationProvider>
 
-        {/* Activity List */}
-        <div className="flex justify-between items-center mb-3">
-          <h2 className="text-lg font-semibold ml-3">Activity</h2>
-          {/* <div>View All</div> */}
-          <Button href="#text-buttons" className="text-sm">
-            View All
-          </Button>
-        </div>
-        <div className="bg-white min-h-50 max-h-82 rounded-xl grow mx-2">
-          <div>Project 1</div>
-          <div>Project 2</div>
-          <div>Project 3</div>
+          {/* Activity List */}
+          <div className="flex justify-between items-center mb-3">
+            <h2 className="text-lg font-semibold ml-3">Activity</h2>
+            {/* <div>View All</div> */}
+            <Button href="#text-buttons" className="text-sm">
+              View All
+            </Button>
+          </div>
+          <div className="bg-white min-h-50 max-h-82 rounded-xl grow mx-2">
+            <div>Project 1</div>
+            <div>Project 2</div>
+            <div>Project 3</div>
+          </div>
         </div>
       </div>
     </div>
