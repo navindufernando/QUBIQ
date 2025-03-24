f728a2ef8b90fa551dcb012ba477845e80b62b1e
Navindu Fernando <fernandojanith15@gmail.com>
2025-03-24 08:51:51 +0530

    Create TabPanel.tsx

---
+++ common/sideandnavbar/src/pages/ProjectReview/reusablecomponents/TabPanel.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+import { TabPanelProps } from "../types";
+
+export default function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ py: 3 }}>{children}</Box>}
+    </div>
+  );
+}
